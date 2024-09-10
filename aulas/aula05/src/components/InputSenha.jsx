c6aef62bdb07f76335e5d0bf00b99a42c6606d76
Wendy <wendyrtn@gmail.com>
2024-09-09 21:54:33 -0300

    aula05

---
+++ aulas/aula05/src/components/InputSenha.jsx
@@ -0,0 +1,11 @@
+function InputSenha(props){
+    return(
+        <>
+            <label htmlFor="senha">Senha</label>
+            <input type="password" id="senha" name="senha" value={props.valor} 
+            onChange={(e)=> props.mudaValor(e.target.value)} required/>
+        </>
+    );
+}
+
+export default InputSenha;
