ed2af67104ab2b854286e8938055da78af1c24e3
Wendy <wendyrtn@gmail.com>
2024-09-26 20:08:41 -0300

    aula06

---
+++ aulas/aula06/src/pages/Perfil.jsx
@@ -0,0 +1,23 @@
+function Perfil() {
+    return (
+        <>
+        <header>
+            <h1>Aluno Online</h1>
+        </header>
+        <main>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Perfil</li>
+                    <li>Sair</li>
+                </ul>
+            </nav>
+            <section>
+                <h2>Perfil do Usuário</h2>
+            </section>
+        </main>
+        </>
+    )
+}
+
+export default Perfil
